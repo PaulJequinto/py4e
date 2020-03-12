66a390922d670595123585f613e6f6a2d6b4a590
Paul Martin P. Jequinto <pauljequinto@Pauls-MacBook-Air.local>
2020-03-13 00:24:57 +0800

    yehey

---
+++ CSexercise3.py
@@ -0,0 +1,17 @@
+try:
+  a=input("Enter score: ")
+  float(a)
+  if float(a) >= 0.9 and float(a) <= 1.0:
+    print("A")
+  elif float(a) >= 0.8 and float(a) <= 0.9:
+    print("B")
+  elif float(a) >= 0.7 and float(a) <= 0.8:
+    print("C")
+  elif float(a) >= 0.6 and float(a) <= 0.7:
+    print("D")
+  elif float(a) > 0 and float(a) <= 0.6:
+    print("F")
+  else:
+    print("Bad score.")
+except NameError, ValueError:
+    print("Bad score.")
