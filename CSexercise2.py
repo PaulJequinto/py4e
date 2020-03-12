53dfd4b42e487cb30f4a5e258f7a61bb5441161b
Paul Martin P. Jequinto <pauljequinto@Pauls-MacBook-Air.local>
2020-03-12 19:15:54 +0800

    yuhoo i got it

--- CSexercise2.py
+++ CSexercise2.py
@@ -1,7 +1,9 @@
-a = float(input ("Enter Hours:"))
-#this is fh
-b = float(input ("Enter Rate:"))
-#this is fr
+try:
+    a = float(input("Enter Hours: "))
+    b = float(input("Enter Rate: "))
+except:
+    print("Error, please enter numeric input")
+    quit()
 if a > 40 :
     print("Overtime")
     reg = a * b
